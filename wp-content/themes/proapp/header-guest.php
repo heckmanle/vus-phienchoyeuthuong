9a27510aa66f78bc6e8abcdce9ab6a2a097ac164
Heckman <heckmanle@gmail.com>
2021-09-06 13:36:07 +0700

    update header

--- wp-content/themes/proapp/header-guest.php
+++ wp-content/themes/proapp/header-guest.php
@@ -133,40 +133,10 @@ if ( ! defined( 'WPINC' ) ) { die; }
                     <div class="app-header__content">
                         <div class="app-header-right">
 
-                            <nav class="navbar navbar-expand-lg">
-
-                                <div class="collapse navbar-collapse">
-
-                                        <?php
-                                        wp_nav_menu(array(
-                                            'container' => '', // Leaving it empty removes the <div> container.
-                                            'menu_class'=> 'navbar-nav mr-auto',
-                                            'add_li_class'  => 'nav-item'
-                                        ));
-                                        ?>
-
-
-                                        <?php /*
-                                        <li class="nav-item active">
-                                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                                        </li>
-
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Dropdown
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item" href="#">Action</a>
-                                                <a class="dropdown-item" href="#">Another action</a>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                            </div>
-                                        </li>
-                                        */ ?>
-
-
-                                </div>
-                            </nav>
+                            <p>
+                                Vietnam USA Society English Centers<br/>
+                                <a href="https://www.vus.edu.vn">www.vus.edu.vn</a>
+                            </p>
 
                         </div>
                     </div>
