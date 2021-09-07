b48a1e1cd7d1326950b5a0c7ada511c74f90970c
Richard <nguyenkha252@gmail.com>
2021-09-07 14:12:57 +0700

    update

--- wp-config.php
+++ wp-config.php
@@ -28,7 +28,7 @@ define( 'DB_NAME', 'vs_phienchoyeuthuong' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', 'vD#154hfknd!.' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
