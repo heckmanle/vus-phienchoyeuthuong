e36f309a8d7cf0f015f38187220ea5a4ef04b0c8
Heckman <heckmanle@gmail.com>
2021-09-21 18:23:11 +0700

    Update page-x.php

--- wp-content/themes/proapp/page-x.php
+++ wp-content/themes/proapp/page-x.php
@@ -244,7 +244,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                     <p class="text-center mb-4"><?php _e('Bạn đang cần hỗ trợ'); ?>:</p>
                     <div class="form-group">
                         <label><?php _e('Combo thực phẩm'); ?></label>
-                        <textarea id="step-1-note-0" class="form-control"><?php _e("Thông tin nhận Combo thực phẩm:\nHọ tên: \nđịa chỉ nhận hàng: "); ?></textarea>
+                        <textarea id="step-1-note-0" class="form-control"><?php _e("Thông tin nhận Combo thực phẩm:\nHọ tên: \nĐịa chỉ nhận hàng: "); ?></textarea>
                     </div>
 
                     <div class="form-group">
